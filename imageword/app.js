1fd9e7c8d76c1786d55c889d1c375e331d2a43de
zenjack <zenjack@foxmail.com>
2016-06-15 23:02:55 +0400

    1.添加設置功能；2.修復本地存儲BUG

--- imageword/app.js
+++ imageword/app.js
@@ -1,17 +1,28 @@
 (function() {
 	var app = angular.module('hanword', []);
+	
+	var wordStore = new StoreBinder('word');
+	var wordQuantityStore = new StoreBinder('wordQuantity');
+	var pageQuantityStore = new StoreBinder('pageQuantity');
+
 
 	app.controller('HanwordCtrl', function() {
-		var store = new Store();
-		var key = 'word';
+		var wordQuantity = wordQuantityStore.get();
+		var pageQuantity = pageQuantityStore.get();
+		if (wordQuantity === null) {
+			wordQuantity = 30;
+		}
+		if (pageQuantity === null) {
+			pageQuantity = 6;
+		}
 
 		this.word = {};
-		this.wordPage = new Page(30, words);
+		this.wordPage = new Page(wordQuantity, words);
 		var pages = [];
 		for (var i = 0; i < this.wordPage.pageSum; i++) {
 			pages.push(i+1);
 		}
-		this.pagerPage = new Page(6, pages);
+		this.pagerPage = new Page(pageQuantity, pages);
 
 		this.isSelectedPage = function(selectedPage) {
 			return this.wordPage.currentPage === selectedPage;
@@ -41,7 +52,7 @@
 				this.word.error = '';
 				this.wordPage.setIndex(index);
 				this.pagerPage.setIndex(Math.ceil((index+1) / this.wordPage.pageSize)-1);
-				store.put(key, name);
+				wordStore.put(name);
 			} else {
 				this.word.name = store.get(key);
 				this.word.error = `提示：系統未收錄【${searchWord}】字`;
@@ -49,7 +60,7 @@
 		}
 		
 		this.init = function() {
-			var name = store.get(key);
+			var name = wordStore.get();
 			if (!name) {
 				name = '日';
 			}
@@ -65,6 +76,24 @@
 		}
 		this.isSelected = function(setTab) {
 			return this.tab === setTab;
+		}
+	});
+
+	app.controller('SetCtrl', function() {
+		this.setter = {};
+		try {
+			this.setter.wordQuantity = wordQuantityStore.get();
+			this.setter.pageQuantity = pageQuantityStore.get();
+		} catch (err) {}
+
+		this.submitSet = function() {
+			wordQuantityStore.put(this.setter.wordQuantity);
+			pageQuantityStore.put(this.setter.pageQuantity);
+			this.setter.tip = '提示：設置成功';
+		}
+
+		this.changeValue = function() {
+			this.setter.tip = '';
 		}
 	});
 })();
--- imageword/lib/js/store.js
+++ imageword/lib/js/store.js
@@ -1,22 +1,54 @@
 function Store() {
-
-}
-Store.prototype.check = function() {
 	if (!localStorage) throw '你使用的瀏覧器不支持本地存儲！';
 }
+
+Store.prototype.checkNull = function(value) {
+	if (value === null || value === undefined) {
+		throw '不允許使用 null 或 undefined 值。';
+	}
+}
+
 Store.prototype.put = function(key, value) {
-	this.check();
-	value = value || '';
+	this.checkNull(value);
 	value = JSON.stringify(value);
-	localStorage[key] = value;
+	localStorage.setItem(key,value);
 }
 
 Store.prototype.get = function(key) {
-	this.check();
-	var value = localStorage[key];
-	if (value) {
-		return JSON.parse(value);
-	} else {
-		return '';
+	var value = localStorage.getItem(key);
+	return JSON.parse(value);
+}
+
+Store.prototype.hasKey = function(key) {
+	for (var i = 0; i < localStorage.length; i++) {
+		if (localStorage.key(i) === key) {
+			return true;
+		}
 	}
+	return false;
+}
+
+Store.prototype.keys = function() {
+	var array = [];
+	for (var i = 0; i < localStorage.length; i++) {
+		array.push(localStorage.key(i));
+	}
+	return array;
+}
+
+Store.prototype.remove = function(key) {
+	localStorage.removeItem(key);
+}
+
+function StoreBinder(key) {
+	this.store = new Store();
+	this.key = key;
+}
+
+StoreBinder.prototype.get = function() {
+	return this.store.get(this.key);
+}
+
+StoreBinder.prototype.put = function(value) {
+	this.store.put(this.key, value);
 }
